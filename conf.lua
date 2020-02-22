f89d555c6f29ae197a83ba589552f2ab5b4169d2
Thomas Holden <tholden@sanfordguide.com>
2020-02-22 17:48:47 -0500

    update game title

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.version = '11.2'
     t.window.resizable = true
-    t.window.title = 'Chione'
+    t.window.title = 'Quincy'
     t.window.width = 1280
     t.window.height = 720
 end
